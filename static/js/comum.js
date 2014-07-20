7ece98bb80a7b98a31771b29ec6f656e46246049
Luan Garcia <luangarcia@MacBook-Air-de-Luan.local>
2014-07-20 18:10:00 -0300

    modal destaque, modal evento e modal produto - nao responsivo ainda

--- static/js/comum.js
+++ static/js/comum.js
@@ -1,91 +1,157 @@
 $(function(){
 
 
-		$("#ico-menu").on({
-			click: function(){
-				var ico = $(this);
-				var html_body = $("html,body");
-				var header = $("#Header");
-				var banner = $("#Header .head-mob");
-				var menu = $("#Menu");
+	$("#ico-menu").on({
+		click: function(){
+			var ico = $(this);
+			var html_body = $("html,body");
+			var header = $("#Header");
+			var banner = $("#Header .head-mob");
+			var menu = $("#Menu");
 
-				var altura_window = $(window).height();
-				
-				if(ico.hasClass("ativo")) {
-					ico.removeClass("ativo");
-					html_body.removeAttr("style");
-				
-					banner.animate({
-						marginLeft: "0"
-					})
-
-					ico.animate({
-						marginLeft: "0"
-					})
-
-					menu.animate({
-						left: "-275px"
-					});
-
-					$(".mask").remove();
-					return false;
-					
-				}
-
-				ico.addClass("ativo");
-
-
-				
-				html_body.css({
-					"overflow":"hidden",
-					"height": altura_window+"px"
-				});
-				
+			var altura_window = $(window).height();
+			
+			if(ico.hasClass("ativo")) {
+				ico.removeClass("ativo");
+				html_body.removeAttr("style");
+			
 				banner.animate({
-					marginLeft: "265px"
+					marginLeft: "0"
 				})
 
 				ico.animate({
-					marginLeft: "265px"
+					marginLeft: "0"
 				})
 
 				menu.animate({
-					left: "0px"
+					left: "-275px"
 				});
 
-				header.append("<div class='mask'></div>")
+				$(".mask").remove();
+				return false;
+				
 			}
-		});
 
-		$(document.body).on({
-			tap: function(){
-				var ico = $("#ico-menu");
-				ico.trigger("click");
+			ico.addClass("ativo");
+
+
+			
+			html_body.css({
+				"overflow":"hidden",
+				"height": altura_window+"px"
+			});
+			
+			banner.animate({
+				marginLeft: "265px"
+			})
+
+			ico.animate({
+				marginLeft: "265px"
+			})
+
+			menu.animate({
+				left: "0px"
+			});
+
+			header.append("<div class='mask'></div>")
+		}
+	});
+
+	$(document.body).on({
+		tap: function(){
+			var ico = $("#ico-menu");
+			ico.trigger("click");
+		}
+	}, ".maskmenu");
+
+	$(".item-menu .lnk").on({
+		click: function(){
+			var link = $(this);
+			var item = link.parent();
+			var submenu = $(".submenu", item);
+			var all_subs = $(".item-menu .submenu");
+			var all_link = $(".item-menu .lnk");
+
+
+			if(link.hasClass("ativo")) {
+				link.removeClass("ativo");
+				submenu.slideUp();
+
+				return false;
 			}
-		}, ".mask");
 
-		$(".item-menu .lnk").on({
-			click: function(){
-				var link = $(this);
-				var item = link.parent();
-				var submenu = $(".submenu", item);
-				var all_subs = $(".item-menu .submenu");
-				var all_link = $(".item-menu .lnk");
+			all_subs.slideUp();
+			all_link.removeClass("ativo");
+
+			link.addClass("ativo");
+			submenu.slideDown();
+		}
+	});
+
+	//disparaModalRequest("modais/produto.html","786","");
+	//disparaModalRequest("modais/destaque.html","786","");
+	disparaModalRequest("modais/evento.html","560","");
+
+	//abre modal
+
+	$("[rel='modal']").on({
+		click: function(){
+			var tamanho = $(this).attr('data-param');
+			var qualclasse = $(this).attr("data-class");
+			disparaModalRequest($(this).attr("href"), tamanho, qualclasse);
+		}
+	});
+
+	//fechar modal
+	$(document.body).on({
+		click: function(){
+			fechaModal();
+		}
+	}, "[data-param='closemodal']");
+
+});
 
 
-				if(link.hasClass("ativo")) {
-					link.removeClass("ativo");
-					submenu.slideUp();
 
-					return false;
-				}
+//FUNCAO PRA ABRIR O LIGHTBOX POR AJAX
+function exibirConteudo(conteudo, tamanho, qualclasse) {
+	$('body').append("<div id='lightbox' class="+qualclasse+">"
+	+"  <div class='mask' data-param='closemodal'></div>"
+	+       "<div class='lightbox' style='width:"+ tamanho +"px; margin-left:-"+ tamanho/2 +"px;'>"
+	+"          <a href='javascript:;' class='fechar_modal rpl' data-param='closemodal' title='fechar'>fechar</a>"
+	+"          <div id='ligbtox_content' class='fix'></div>"
+	+"      </div>"
+	+"  </div>");
 
-				all_subs.slideUp();
-				all_link.removeClass("ativo");
+	$('#ligbtox_content').append(conteudo);
+}
 
-				link.addClass("ativo");
-				submenu.slideDown();
-			}
-		})
+/*Usado para carregar o lightbox a partir de uma url*/
 
-})
+// PASSA UMA URL PRO LIGHTBOX ABRIR
+function disparaModalRequest(url, tamanho, qualclasse) {
+	// console.log("disparaModalRequest: "+url+"|"+tamanhp);
+	$.ajax({
+		url: url,
+		data:'html',
+		beforeSend: function(){
+			$('body').append("<div class='lightbox preloading'><p class='loading-modal'><img src='"+static_url+"img/loader-bgescuro.gif' alt='' class='fl' /> <span class='fl'>Carregando</span></p></div>");
+			// return false;
+		},
+		success: function(conteudo){
+			$(".lightbox").remove();
+			$("body, html").css("overflow","hidden");
+			exibirConteudo(conteudo, tamanho, qualclasse);
+		}
+	});
+
+	return false;
+}
+
+// FECHA O MODAL AJAX
+function fechaModal() {
+	$("body").find("#lightbox").fadeTo("normal", 0, function(){
+		$(this).remove().hide();
+		$("body, html").removeAttr("style");
+	});
+}
