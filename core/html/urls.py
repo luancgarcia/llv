3837d6b4ed9cf1796a7f5a126417762b7d5c05f0
Jonatas C Damasceno <jonatas.cd@gmail.com>
2014-07-24 18:45:15 -0300

    pequeno ajuste nas urls de html

--- core/html/urls.py
+++ core/html/urls.py
@@ -8,19 +8,19 @@ urlpatterns = patterns('',
         'html.views.html_grid',
         name='html_grid'),
 
-   	url(r'home/$',
+    url(r'home/$',
         'html.views.html_home',
         name='html_home'),
 
-   	url(r'^home/modais/produto',
+    url(r'^home/modais/produto',
         'html.views.html_modal_produtos',
         name='html_modal_produtos'),
 
-   	url(r'^home/modais/destaque',
+    url(r'^home/modais/destaque',
         'html.views.html_modal_destaque',
         name='html_modal_destaque'),
 
-   	url(r'^home/modais/evento',
+    url(r'^home/modais/evento',
         'html.views.html_modal_evento',
         name='html_modal_evento'),
 )
