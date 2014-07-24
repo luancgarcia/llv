e5b2426cf728b3bb5311c3d14f1394b309e95da0
Jonatas C Damasceno <jonatas.cd@gmail.com>
2014-07-24 18:35:17 -0300

    ajuste no modelo Loja

--- core/lojas/models.py
+++ core/lojas/models.py
@@ -7,8 +7,8 @@ from utils.models import EditorialModel, BaseManager
 
 class Loja(EditorialModel):
     nome = models.CharField(u'Nome', max_length=100, null=True, blank=False)
-    logo = models.CharField(u'Logo', max_length=100, null=True, blank=False)
-    telefone = models.CharField(u'telefone', max_length=100, null=True, blank=False)
+    logo = models.CharField(u'Logo', max_length=100, null=True, blank=True)
+    telefone = models.CharField(u'telefone', max_length=100, null=True, blank=True)
 
     class Meta:
         verbose_name=u'Loja'
