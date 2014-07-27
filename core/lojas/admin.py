f8890feb3cc7911cc4601333288f2b0113bc34f2
Jonatas C Damasceno <jonatas.cd@gmail.com>
2014-07-27 00:50:12 -0300

    ajusta admin de lojas

--- core/lojas/admin.py
+++ core/lojas/admin.py
@@ -4,4 +4,9 @@ from django.contrib import admin
 
 from lojas.models import Loja
 
-admin.site.register(Loja)
+class LojaAdmin(admin.ModelAdmin):
+	list_display = ['nome','telefone','publicada']
+	list_filter = ['publicada']
+	list_editable = ['publicada']
+
+admin.site.register(Loja, LojaAdmin)
