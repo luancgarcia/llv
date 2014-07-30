0f2eddb4433f635ed24a28693c1261f83b986631
Jonatas C Damasceno <jonatas.cd@gmail.com>
2014-07-30 00:12:01 -0300

    merge com html

---
+++ core/html/static/js/cria-elemento.js
@@ -0,0 +1,15 @@
+document.createElement("header");
+document.createElement("footer");
+document.createElement("section");
+document.createElement("aside");
+document.createElement("nav");
+document.createElement("article");
+document.createElement("hgroup");
+document.createElement("time");
+document.createElement("menu");
+document.createElement("figure");
+document.createElement("figcaption");
+document.createElement("details");
+document.createElement("main"); 
+document.createElement("svg"); 
+document.createElement("canvas");
