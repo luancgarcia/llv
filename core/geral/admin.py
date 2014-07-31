43e361ecb3d3d3444153c047f90459c7d81c7ce8
Jonatas C Damasceno <jonatas.cd@gmail.com>
2014-07-31 00:09:04 -0300

    ajusta admin das apps geral e lojas

--- core/geral/admin.py
+++ core/geral/admin.py
@@ -2,13 +2,8 @@
 
 from django.contrib import admin
 
-from geral.models import Shopping, Categoria, Oferta, ImagemOferta, Log, Destaque, Evento
+from geral.models import Categoria, Oferta, ImagemOferta, Log, Destaque, Evento
 
-class ShoppingAdmin(admin.ModelAdmin):
-    list_display = ['nome','publicada','id_multiplan']
-    prepopulated_fields = {'slug': ('nome',), }
-    list_editable = ['publicada']
-    list_filter = ['publicada']
 
 class CategoriaAdmin(admin.ModelAdmin):
     list_display = ['nome','publicada']
@@ -95,7 +90,6 @@ class ImagemOfertaAdmin(admin.ModelAdmin):
     exclude = ['principal','vertical']
 
 
-admin.site.register(Shopping, ShoppingAdmin)
 admin.site.register(Categoria, CategoriaAdmin)
 admin.site.register(Oferta, OfertaAdmin)
 # admin.site.register(Destaque, DestaqueAdmin)
--- core/lojas/admin.py
+++ core/lojas/admin.py
@@ -2,11 +2,18 @@
 
 from django.contrib import admin
 
-from lojas.models import Loja
+from lojas.models import Loja, Shopping
 
 class LojaAdmin(admin.ModelAdmin):
 	list_display = ['nome','telefone','publicada']
 	list_filter = ['publicada']
 	list_editable = ['publicada']
 
+class ShoppingAdmin(admin.ModelAdmin):
+    list_display = ['nome','publicada','id_multiplan']
+    prepopulated_fields = {'slug': ('nome',), }
+    list_editable = ['publicada']
+    list_filter = ['publicada']
+
 admin.site.register(Loja, LojaAdmin)
+admin.site.register(Shopping, ShoppingAdmin)
