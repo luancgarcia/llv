5ce00539f1673f629422768d5e50d580bba8cc2f
Jonatas C Damasceno <jonatas.cd@gmail.com>
2014-07-31 01:32:39 -0300

    avança na view home

--- core/geral/views.py
+++ core/geral/views.py
@@ -7,7 +7,15 @@ from geral.models import Categoria, ImagemOferta, Oferta, Log
 from lojas.models import Loja
 
 def home(request):
-    contexto = {'lojas': Loja.objects.all()}
+    destaques = Oferta.destaques_prontos()
+    eventos = Oferta.eventos_prontos()
+    total_destaques = len(destaques)
+    total_eventos = len(eventos)
+
+    contexto = {'lojas': Loja.objects.all(),
+                'destaques': destaques,
+                'eventos': eventos,
+                'ofertas': Oferta.ofertas_prontas()}
     return render(request, "home.html", contexto)
 
 def modal(request):
