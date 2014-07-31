10fc2557eb3222a799b62e6471a9e9f4f76d7315
Jonatas C Damasceno <jonatas.cd@gmail.com>
2014-07-31 02:03:28 -0300

    cria tamanho de imagem relativo a oferta

--- core/geral/models.py
+++ core/geral/models.py
@@ -143,6 +143,10 @@ class ImagemOferta(OrderedModel):
                                  resize.ResizeToFill(376, 376)],
                                  source='imagem', format='PNG',
                                  options={'quality': 90})
+    img_172x172 = ImageSpecField([Adjust(contrast=1.1, sharpness=1.1),
+                                 resize.ResizeToFill(172, 172)],
+                                 source='imagem', format='PNG',
+                                 options={'quality': 90})
     evento_180x445 = ImageSpecField([Adjust(contrast=1.1, sharpness=1.1),
                                  resize.ResizeToFill(180, 445)],
                                  source='imagem', format='PNG',
