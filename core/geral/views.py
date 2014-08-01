f175c6c97255e62e4f3b4ed023239a036e26ff24
Jonatas C Damasceno <jonatas.cd@gmail.com>
2014-08-01 00:14:17 -0300

    método para calcular slice total de ofertas

--- core/geral/views.py
+++ core/geral/views.py
@@ -6,6 +6,12 @@ from django.views.decorators.csrf import csrf_exempt
 from geral.models import Categoria, ImagemOferta, Oferta, Log
 from lojas.models import Loja
 
+
+def slice_oferta(total_destaques, total_eventos):
+    total_destaques = total_destaques * 4
+    total_eventos = total_eventos * 2
+    return 32 - total_destaques - total_eventos
+
 def home(request):
     destaques = Oferta.destaques_prontos()
     eventos = Oferta.eventos_prontos()
