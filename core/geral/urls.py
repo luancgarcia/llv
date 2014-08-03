014a81779479b5082504f2228e8fd8adc2a56b25
Jonatas C Damasceno <jonatas.cd@gmail.com>
2014-08-03 17:17:22 -0300

    ajuste url de modal

--- core/geral/urls.py
+++ core/geral/urls.py
@@ -11,21 +11,21 @@ urlpatterns = patterns('',
     # url(r'^$', 'core.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^html/', include('html.urls')),
+    # url(r'^html/', include('html.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^modais/produto',
-        'html.views.html_modal_produtos',
-        name='html_modal_produtos'),
+    url(r'^modais/(?P<tipo>[\w-]+)/(?P<id_item>\d+)/$',
+        'geral.views.modal',
+        name='modal'),
 
-    url(r'^modais/destaque',
-        'html.views.html_modal_destaque',
-        name='html_modal_destaque'),
+    # url(r'^modais/destaque/(?P<id_item>\d+)/$',
+    #     'geral.views.modal',
+    #     name='modal_destaque'),
 
-    url(r'^modais/evento',
-        'html.views.html_modal_evento',
-        name='html_modal_evento'),
+    # url(r'^modais/evento/(?P<id_item>\d+)/$',
+    #     'geral.views.modal',
+    #     name='modal_evento'),
 
     url(r'^modais/share',
         'html.views.html_modal_share',
