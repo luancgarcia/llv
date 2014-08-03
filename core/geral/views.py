b34c9553c330373f084fe13d48400b373f9b93ba
Jonatas C Damasceno <jonatas.cd@gmail.com>
2014-08-03 15:53:04 -0300

    implementa mais_ofertas

--- core/geral/views.py
+++ core/geral/views.py
@@ -13,31 +13,55 @@ def slice_oferta(total_destaques, total_eventos):
     total_eventos = total_eventos * 2
     return 32 - total_destaques - total_eventos
 
+def ultimo_id(lista):
+    ultimo_id = 0
+    for i in lista:
+        if i['id'] > ultimo_id:
+            ultimo_id = int(i['id'])
+    return ultimo_id if ultimo_id > 0 else ''
+
 def home(request):
     destaques = Oferta.prontos(tipo=Oferta.DESTAQUE)
+
     eventos = Oferta.prontos(tipo=Oferta.EVENTO)
 
     ofertas = Oferta.prontos()
     ofertas = ofertas[:slice_oferta(len(destaques),len(eventos))]
 
-
     contexto = {'lojas': Loja.objects.all(),
                 'destaques': destaques,
-                'ultimo_destaque_id': destaques[::-1][0]['id'],
+                'ultimo_destaque_id': ultimo_id(destaques),
                 'eventos': eventos,
-                'ultimo_evento_id': eventos[::-1][0]['id'],
+                'ultimo_evento_id': ultimo_id(eventos),
                 'ofertas': ofertas,
-                'ultima_oferta_id': ofertas[::-1][0]['id']}
+                'ultima_oferta_id': ultimo_id(ofertas)}
     return render(request, "home.html", contexto)
 
 @csrf_exempt
 def mais_ofertas(request):
-    ultimo_destaque = request.POST.get('ultimo_destaque')
-    ultimo_evento = request.GET.get('ultimo_evento')
-    ultima_oferta = request.GET.get('ultima_oferta')
-    if not any([ultimo_destaque,ultimo_evento,ultima_oferta]):
-        raise Http404
-    return
+    ultimo_destaque = request.POST.get('ultimo_destaque', None)
+    ultimo_evento = request.POST.get('ultimo_evento', None)
+    ultima_oferta = request.POST.get('ultima_oferta', None)
+
+    if not all([ultimo_destaque,ultimo_evento,ultima_oferta]):
+        contexto = {}
+    else:
+        destaques = Oferta.prontos(tipo=Oferta.DESTAQUE, from_id=ultimo_destaque)
+
+        eventos = Oferta.prontos(tipo=Oferta.EVENTO, from_id=ultimo_evento)
+
+        ofertas = Oferta.prontos(from_id=ultima_oferta)
+        ofertas = ofertas[:slice_oferta(len(destaques),len(eventos))]
+
+        contexto = {'lojas': Loja.objects.all(),
+                    'destaques': destaques,
+                    'ultimo_destaque_id': ultimo_id(destaques),
+                    'eventos': eventos,
+                    'ultimo_evento_id': ultimo_id(eventos),
+                    'ofertas': ofertas,
+                    'ultima_oferta_id': ultimo_id(ofertas)}
+
+    return render(request, "home-part.html", contexto)
 
 def modal(request, tipo, id_item):
     return
