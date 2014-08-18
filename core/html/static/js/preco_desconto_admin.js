1d4da78840cf83607049b510304fb62de9dbd474
Jonatas C Damasceno <jonatas.cd@gmail.com>
2014-08-18 00:07:41 -0300

    melhora calculo de porcentagem no admin

--- core/html/static/js/preco_desconto_admin.js
+++ core/html/static/js/preco_desconto_admin.js
@@ -7,19 +7,20 @@ $(document).ready(function(){
     $(".field-preco_inicial").width('300').css('float',"left");
     $(".field-preco_final").width('300').css('float',"left");
 
-    $("#id_preco_inicial").maskMoney({thousands:'', decimal:','});
-    $("#id_preco_final").maskMoney({thousands:'', decimal:','});
-
-    var inicial = parseInt($("#id_preco_inicial").val().replace(',','.'));
-    var pfinal = parseInt($("#id_preco_final").val().replace(',','.'));
+    $("#id_preco_inicial").maskMoney({thousands:'.', decimal:','});
+    $("#id_preco_final").maskMoney({thousands:'.', decimal:','});
 
     $(function() {
         $("#id_preco_inicial, #id_preco_final").keypress(function() {
-            if ($("#id_preco_inicial").val() > $("#id_preco_final").val()){
-                var result = parseInt(parseFloat($("#id_preco_final").val(), 10) * 100)/ parseFloat($("#id_preco_inicial").val(), 10);
+            var inicial = parseFloat($("#id_preco_inicial").val().replace('.','').replace(',','.'));
+            var pfinal = parseFloat($("#id_preco_final").val().replace('.','').replace(',','.'));
+
+            if (inicial > pfinal){
+                var result = parseInt((pfinal * 100)/ inicial);
                 var result_str = result+'';
                 // $('#id_desconto').val(result_str.substr(0,2)+'%');
-                $('#id_desconto').val(result_str.substr(0,2));
+                // $('#id_desconto').val(result_str.substr(0,2));
+                $('#id_desconto').val(result);
             }else{
                 $('#id_desconto').val('-');
             }
