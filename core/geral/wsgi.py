5018c421edc7ba9ae6be2d0b758195c2f7151785
Jonatas C Damasceno <jonatas.cd@gmail.com>
2014-08-24 04:37:45 -0300

    ajusta wsgi.py

--- core/geral/wsgi.py
+++ core/geral/wsgi.py
@@ -7,8 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
+#!/home/liquidacaolapisv/public_html/.virtualenvs/llv/bin/activate
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+import sys
+import site
+
+site_packages = '/home/liquidacaolapisv/public_html/.virtualenvs/llv/lib/python2.7/site-packages'
+site.addsitedir(os.path.abspath(site_packages))
+
+sys.path.insert(0, '/home/liquidacaolapisv/public_html/llv/core')
+sys.path.insert(0, '/home/liquidacaolapisv/public_html/llv/core/geral')
+sys.path.insert(0, '/home/liquidacaolapisv/public_html/.virtualenvs/llv/lib/python2.7/site-packages')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.geral.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
