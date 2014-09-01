cc97ad3d8d206e2820f1f118b290085516063d79
Jonatas C Damasceno <jonatas.cd@gmail.com>
2014-09-01 13:15:06 -0300

    refatora decorator de shopping

--- core/geral/decorators.py
+++ core/geral/decorators.py
@@ -5,18 +5,16 @@ from .models import Shopping
 def indica_shopping(funcao):
     def _wrap(*args, **kwargs):
         request = args[0]
-        chave_shooping = kwargs.get('shp_id', None)
-
-        if not chave_shooping:
-            slug = request.GET.get('shopping', None)
-            if not slug:
-                kwargs['shp_id'] = 1
-            else:
-                shopping = Shopping.objects.filter(slug=slug)[:1]
-                if shopping:
-                    kwargs['shp_id'] = shopping[0].id
+        cookie_shopping = request.COOKIES.get('shp_id', None)
+        slug = request.GET.get('shopping', None)
+        if slug:
+            shopping = Shopping.objects.filter(slug=slug)[:1]
+            if shopping:
+                kwargs['shp_id'] = shopping[0].id
+        elif cookie_shopping:
+            kwargs['shp_id'] = cookie_shopping
         else:
-            kwargs['shp_id'] = chave_shooping
+            kwargs['shp_id'] = 1
 
         return funcao(*args, **kwargs)
     return _wrap
