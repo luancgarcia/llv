107bca5b07883157c94c6760697c4b45f898cbfb
Jonatas C Damasceno <jonatas.cd@gmail.com>
2014-10-03 08:13:27 -0300

    ajuste na view modal_share

--- core/geral/views.py
+++ core/geral/views.py
@@ -327,7 +327,9 @@ def modal_share(request, id_item):
     item = Oferta.objects.get(id=id_item)
     imagem = item.imagens.all()[:1].get()
     sazonal = Sazonal.atual(serializado=False)
-    imagens_sazonal = [m.to_dict() for m in sazonal.mascaras.all()]
+    imagens_sazonal = []
+    if sazonal:
+        imagens_sazonal = [m.to_dict() for m in sazonal.mascaras.all()]
     contexto = {'item': item.to_dict(modal=True),
                 'imagem_url': imagem.img_376x376.url,
                 'imagem_id': imagem.id,
