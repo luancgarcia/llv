a3873c1aea55b53088a1c1c6528a9d0d9a4700ba
Jonatas C Damasceno <jonatas.cd@gmail.com>
2014-10-05 23:00:02 -0300

    ajuste no admin de Notificações

--- core/notificacoes/admin.py
+++ core/notificacoes/admin.py
@@ -8,7 +8,8 @@ from notificacoes.models import Notificacao, Solicitacao
 class NotificacaoAdmin(admin.ModelAdmin):
     exclude = ['publicada']
     list_filter = ['lida','resolvida']
-    list_display = ['oferta','lida','resolvida','enviada_mkt','enviada_lojista']
+    list_display = ['solicitante', 'responsavel', 'oferta', 'lida', 'resolvida',
+                    'enviada_mkt', 'enviada_lojista']
     list_editable = ['lida']
 
 
