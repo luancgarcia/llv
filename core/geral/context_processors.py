f5abd82e5668a58c788ea7f30e70610844e2f7db
Jonatas C Damasceno <jonatas.cd@gmail.com>
2014-11-18 16:19:46 -0200

    atualiza versão dos estáticos

--- core/geral/context_processors.py
+++ core/geral/context_processors.py
@@ -7,5 +7,5 @@ def context(request):
     '''
     return {'SITE_URL': settings.SITE_URL,
             'STATIC_URL': settings.STATIC_URL,
-            'VERSAO_ESTATICOS': 11,
+            'VERSAO_ESTATICOS': 12,
             'FACEBOOK_APP_ID': settings.FACEBOOK_APP_ID}
