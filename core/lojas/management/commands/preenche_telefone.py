0bc818b33828627cec3aad9c765eea30dd269936
Jonatas C Damasceno <jonatas.cd@gmail.com>
2015-02-02 13:59:56 -0200

    cria manage para preencher telefone das lojas

---
+++ core/lojas/management/commands/preenche_telefone.py
@@ -0,0 +1,51 @@
+# -*- encoding: utf-8 -*-
+
+from optparse import make_option
+from suds.client import Client
+import string
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from lojas.models import Shopping, Loja
+
+
+class Command(BaseCommand):
+    '''
+    Preenche o telefone das lojas consultando o servido do wiseit
+    '''
+    help = 'Preenche o telefone das lojas consultando o servido do wiseit'
+
+    def handle(self, *args, **options):
+        print 45 * '-'
+        print '\tIniciando consulta ao WiseIt'
+        print 45 * '-'
+        try:
+            cliente = Client(settings.WSDL_URL)
+            if cliente:
+                print ' Contectou'
+                shoppings = Shopping.get_publicadas()
+                print ' Iterando sobre %s shoppings' % shoppings.count()
+                for shopping in shoppings:
+                    print ' Buscando lojas do %s' % shopping
+                    dados = cliente.service.findLoja(codEmpreendimento=shopping.id_multiplan)
+                    total = 0
+                    for d in dados:
+                        if d.fantasia.nome:
+                            loja, criada = Loja.objects.get_or_create(shopping=shopping,
+                                                                      nome=string.capwords(d.fantasia.nome))
+                            if loja:
+                                loja.publicada = True
+                                loja.id_multilan = d.codPessoa
+                                loja.save()
+                            total += 1
+                    print '    Total de %s lojas' % total
+            else:
+                print ' Houve algum problema.'
+                print ' Encerrando...'
+
+        except Exception, e:
+            raise e
+        print 45 * '-'
+        print '\tFIM'
+        print 45 * '-'
