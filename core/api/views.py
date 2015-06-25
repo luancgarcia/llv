7bf74d6a7c7822de47c3f85e44cedb74f1f0f887
Jonatas C Damasceno <jonatas.cd@gmail.com>
2015-06-25 13:21:30 -0300

    cria view para api

--- core/api/views.py
+++ core/api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+# -*- encoding: utf-8 -*-
+import simplejson as json
 
-# Create your views here.
+from django.http import HttpResponse
+
+from geral.models import Oferta, Shopping
+
+
+def retorno(dados, tipo):
+    if tipo and tipo.lower() == 'xml':
+        return HttpResponse(json.dumps(dados), content_type="text/xml")
+    else:
+        return HttpResponse(json.dumps(dados), content_type="application/json")
+
+def ofertas(request):
+    slug = request.GET.get('slug', None)
+    tipo = request.GET.get('type', None)
+    dados = {'error': None, 'error_message': None}
+    if not slug:
+        dados.update({'error': 'Shopping não informado',
+                      'error_message': 'Favor informe a slug do shopping. Ex. barra-shopping'})
+    else:
+        shopping = Shopping.objects.get(slug=slug)
+        dados.update({'ofertas': Oferta.prontos_api(shopping=shopping.id)})
+    return retorno(dados, tipo)
