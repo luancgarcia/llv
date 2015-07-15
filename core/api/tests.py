052af1cdb2dad195162560e46fe1ca454861dc94
Jonatas C Damasceno <jonatas.cd@gmail.com>
2015-07-15 19:16:47 -0300

    inicia testes de api

--- core/api/tests.py
+++ core/api/tests.py
@@ -1,3 +1,11 @@
+# -*- encoding: utf-8 -*-
 from django.test import TestCase
 
-# Create your tests here.
+from models import ApiUser
+
+class ApiUserModelTest(TestCase):
+    @classmethod
+    def _create_user(cls):
+        user = ApiUser()
+        #todo: implementar demais campos
+        return user
