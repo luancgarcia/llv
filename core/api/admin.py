e131d21c67799a40fc92067da37a976a5ad7d3ce
Jonatas C Damasceno <jonatas.cd@gmail.com>
2015-07-15 22:50:15 -0300

    habilita admin de api

--- core/api/admin.py
+++ core/api/admin.py
@@ -1,3 +1,14 @@
+# -*- encoding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from models import ApiUser, ApiSession, ApiLog
+
+class ApiUserAdmin(admin.ModelAdmin):
+    readonly_fields = ['token']
+    list_display = ['nome', 'email', 'shopping']
+    list_filter = ['shopping']
+
+
+admin.site.register(ApiUser, ApiUserAdmin)
+admin.site.register(ApiSession)
+admin.site.register(ApiLog)
