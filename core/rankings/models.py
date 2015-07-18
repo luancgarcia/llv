4bee97548c0507e512603fdf2f508961c4a90bb5
Jonatas C Damasceno <jonatas.cd@gmail.com>
2015-07-18 03:25:42 -0300

    fix nos campos de pontuação em Ponto

--- core/rankings/models.py
+++ core/rankings/models.py
@@ -16,14 +16,14 @@ class Intervalo(BaseModel):
 class Ponto(BaseModel):
     intervalo = models.ForeignKey(Intervalo, verbose_name='Intervalo', related_name='pontos')
     loja = models.ForeignKey(Loja, verbose_name='Loja', related_name='pontos')
-    produtos = models.IntegerField(u'Produtos')  # 1 ponto cada
-    fotos = models.IntegerField(u'Fotos')  # 1 ponto cada
-    likes = models.IntegerField(u'Curtidas')  # 1 ponto cada
-    shares = models.IntegerField(u'Compartilhadas')  # 2 pontos cada
-    desconto_30 = models.IntegerField(u'Descontos até 30%') # 1 ponto cada
-    desconto_50 = models.IntegerField(u'Descontos de 31% a 50%') # 2 pontos cada
-    desconto_70 = models.IntegerField(u'Descontos de 51% a 70%') # 3 pontos cada
-    desconto_100 = models.IntegerField(u'Descontos acima de 70%') # 4 pontos cada
+    produtos = models.IntegerField(u'Produtos', null=True)  # 1 ponto cada
+    fotos = models.IntegerField(u'Fotos', null=True)  # 1 ponto cada
+    likes = models.IntegerField(u'Curtidas', null=True)  # 1 ponto cada
+    shares = models.IntegerField(u'Compartilhadas', null=True)  # 2 pontos cada
+    desconto_30 = models.IntegerField(u'Descontos até 30%', null=True) # 1 ponto cada
+    desconto_50 = models.IntegerField(u'Descontos de 31% a 50%', null=True) # 2 pontos cada
+    desconto_70 = models.IntegerField(u'Descontos de 51% a 70%', null=True) # 3 pontos cada
+    desconto_100 = models.IntegerField(u'Descontos acima de 70%', null=True) # 4 pontos cada
 
     class Meta:
         verbose_name = u'Ponto'
