2d628c80d33dc26f2b80f565dc9624c37f0e1df7
Jonatas C Damasceno <jonatas.cd@gmail.com>
2015-07-18 04:59:17 -0300

    ajustes no admin de Ponto

--- core/rankings/admin.py
+++ core/rankings/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from models import Intervalo, Ponto
 
+class PontoAdmin(admin.ModelAdmin):
+    list_filter = ['loja__shopping']
+    list_display = ['loja', 'total']
+    ordering = ['-total']
+
 admin.site.register(Intervalo)
-admin.site.register(Ponto)
+admin.site.register(Ponto, PontoAdmin)
