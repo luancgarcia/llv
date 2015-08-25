f79a732d67470b8a6989839e47ae20d04a76a357
Jonatas C Damasceno <jonatas.cd@gmail.com>
2015-08-25 12:15:16 -0300

    ajusta view do ranking

--- core/rankings/views.py
+++ core/rankings/views.py
@@ -2,14 +2,16 @@
 from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
 
-from models import Ponto
+from models import Intervalo, Ponto
 from lojas.models import Shopping
 
 @staff_member_required
 def ranking_index(request):
     dados = []
+    intervalo = Intervalo.objects.latest('id')
     for s in Shopping.objects.all():
+        pontos = Ponto.dez_mais(s.id, intervalo.id)
         dados.append({'nome': s.nome,
-                      'lojas': [p.to_dict() for p in Ponto.dez_mais(s.id)]})
+                      'lojas': [p.to_dict() for p in pontos]})
 
-    return render(request, "geral.html", {'shoppings': dados})
+    return render(request, "geral.html", {'shoppings': dados, 'intervalo': intervalo})
