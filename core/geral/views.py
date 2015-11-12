2d4022a147f9ee348e72c2b6169f6c0574ea37b0
Jonatas C Damasceno <jonatas.cd@gmail.com>
2015-11-12 01:26:57 -0200

    usa cache na view mais_items

--- core/geral/views.py
+++ core/geral/views.py
@@ -7,6 +7,7 @@ from datetime import date, timedelta, datetime
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.http import Http404
+from django.core.cache import cache
 from django.conf import settings
 from django.db.models import Q
 from aggregate_if import Count
@@ -312,21 +313,29 @@ def mais_items(ids_para_filtrar, tipo, id_shopping):
         corte = 32
     else:
         corte = 3
-    items = Oferta.objects.filter(loja__shopping_id=id_shopping,
-                                  tipo=tipo,
-                                  status=Oferta.PUBLICADO,
-                                  inicio__lte=hoje, fim__gte=hoje) \
-                           .exclude(id__in=ids_para_filtrar) \
-                           .order_by('-data_aprovacao')[:corte]
-    # items = Oferta.objects.filter(tipo=tipo,status=Oferta.PUBLICADO)\
-    #                       .filter(Q(loja__shopping_id=id_shopping) |
-    #                               Q(shopping_id=id_shopping)) \
-    #                       .filter(inicio__lte=hoje,fim__gte=hoje) \
-    #                       .exclude(id__in=ids_para_filtrar) \
-    #                       .order_by('-data_aprovacao')[:corte]
-    items_final = []
-    for i in items:
-        items_final.append(i.to_dict())
+
+    cache_key = 'MAIS_%s_%s_%s' % (id_shopping, tipo, ''.join(map(str, ids_para_filtrar)))
+    cache_mais = cache.get(cache_key)
+
+    if cache_mais:
+        return cache_mais
+    else:
+        items = Oferta.objects.filter(loja__shopping_id=id_shopping,
+                                      tipo=tipo,
+                                      status=Oferta.PUBLICADO,
+                                      inicio__lte=hoje, fim__gte=hoje) \
+                               .exclude(id__in=ids_para_filtrar) \
+                               .order_by('-data_aprovacao')[:corte]
+        # items = Oferta.objects.filter(tipo=tipo,status=Oferta.PUBLICADO)\
+        #                       .filter(Q(loja__shopping_id=id_shopping) |
+        #                               Q(shopping_id=id_shopping)) \
+        #                       .filter(inicio__lte=hoje,fim__gte=hoje) \
+        #                       .exclude(id__in=ids_para_filtrar) \
+        #                       .order_by('-data_aprovacao')[:corte]
+        items_final = []
+        for i in items:
+            items_final.append(i.to_dict())
+        cache.set(cache_key, items_final)
 
     return items_final
 
