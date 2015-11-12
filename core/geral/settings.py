e259112ab8e1451a076c6295f921c636881bd435
Jonatas C Damasceno <jonatas.cd@gmail.com>
2015-11-12 01:22:04 -0200

    habilita cache no settings

--- core/geral/settings.py
+++ core/geral/settings.py
@@ -63,6 +63,13 @@ STATICFILES_DIRS = (
     # os.path.join(BASE_DIR, "static"),
 )
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
